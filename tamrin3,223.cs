dddc9eb055341d0316cdfbaabcdadde2ca8752e3
Zmani844 <mohamadxray907@gmail.com>
2025-05-07 22:03:07 +0330

    Add files via upload

---
+++ tamrin3,223.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adad ra vard konid");
+
+            double c = double.Parse(Console.ReadLine());
+
+            double random = Math.Round(c, 2);
+            Console.WriteLine("adad=" + random);
+        }
+    }
+}
